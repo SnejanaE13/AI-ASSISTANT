d631e87636e03434cb76488287ad5c97b982dd59
sofiavbaranova <sofia.v.baranova@gmail.com>
2025-11-07 18:03:31 +0300

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -9,17 +9,11 @@ DB_DIR.mkdir(parents=True, exist_ok=True)
 DB_PATH = DB_DIR / "main.db"                          
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
-<<<<<<< HEAD
 from app.core.config import settings
 
 engine = create_engine(
     settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-=======
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
->>>>>>> 494eac2 (Подключена LLM)
-)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
