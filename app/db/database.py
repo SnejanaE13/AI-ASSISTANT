ef5a6fedd9e7a0a1064d3da27f35c1a37bf080c5
Aleksei <llexxass@gmail.com>
2025-11-08 20:14:13 +0300

    minimal fixes

--- app/db/database.py
+++ app/db/database.py
@@ -12,7 +12,7 @@ SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 from app.core.config import settings
 
 engine = create_engine(
-    settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
